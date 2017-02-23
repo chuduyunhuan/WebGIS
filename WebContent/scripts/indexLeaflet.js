ac3552b3c2a47b686087d0a998ff9b1a58543f07
devdongx <vitality-xing@163.com>
2017-02-23 16:09:46 +0800

    新增对象数组排序

--- WebContent/scripts/indexLeaflet.js
+++ WebContent/scripts/indexLeaflet.js
@@ -39,6 +39,40 @@
  	}
  	return true;
  };
+ /*
+ * 快速排序，按某个属性，或按“获取排序依据的函数”，来排序.
+ * @method sortByField
+ * @static
+ * @param {array} arr 待处理数组
+ * @param {string|function} prop 排序依据属性，获取
+ * @param {boolean} desc 降序
+ * @return {array} 返回排序后的新数组
+ */
+ var sortByField = function(arr, prop, desc) {
+ 		var props = [],
+ 			ret = [],
+ 			i = 0,
+ 			len = arr.length;
+ 		if (typeof prop == 'string') {
+ 			for (; i < len; i++) {
+ 				var oI = arr[i];
+ 				(props[i] = new String(oI && oI[prop] || ''))._obj = oI;
+ 			}
+ 		} else if (typeof prop == 'function') {
+ 			for (; i < len; i++) {
+ 				var oI = arr[i];
+ 				(props[i] = new String(oI && prop(oI) || ''))._obj = oI;
+ 			}
+ 		} else {
+ 			throw '参数类型错误';
+ 		}
+ 		props.sort();
+ 		for (i = 0; i < len; i++) {
+ 			ret[i] = props[i]._obj;
+ 		}
+ 		if (desc) ret.reverse();
+ 		return ret;
+ };
 //命名空间
 var BASE_MAP = {
 	map: {},
