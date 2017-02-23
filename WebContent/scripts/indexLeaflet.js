13816e785d2511cdefece03f73e9f9d8dfbb00ad
devdongx <vitality-xing@163.com>
2017-02-23 16:35:53 +0800

    新增数字格式化

--- WebContent/scripts/indexLeaflet.js
+++ WebContent/scripts/indexLeaflet.js
@@ -39,6 +39,18 @@
  	}
  	return true;
  };
+ //格式化数字千分位显示，并保存指定位数的小数
+ function fmoney(s, n) {
+ 	n = n ? n : 2;
+ 	s = parseFloat((s + "").replace(/[^\d\.-]/g, "")).toFixed(n) + "";
+ 	var l = s.split(".")[0].split("").reverse(),
+ 		r = s.split(".")[1];
+ 	t = "";
+ 	for (i = 0; i < l.length; i++) {
+ 		t += l[i] + ((i + 1) % 3 == 0 && (i + 1) != l.length ? "," : "");
+ 	}
+ 	return t.split("").reverse().join("") + "." + r;
+ }
  /*
  * 快速排序，按某个属性，或按“获取排序依据的函数”，来排序.
  * @method sortByField
