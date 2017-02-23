eec325c5b3e3b526c22913d903239907f9601fc4
devdongx <vitality-xing@163.com>
2017-02-23 15:27:51 +0800

    比较对象是否相等

--- WebContent/scripts/indexLeaflet.js
+++ WebContent/scripts/indexLeaflet.js
@@ -4,6 +4,41 @@
  * @date    2016-09-22 15:25:49
  * @version $Id$
  */
+ //判断两个对象是否相等
+ Object.prototype.equals = function(x) {
+ 	var p;
+ 	for (p in this) {
+ 		if (typeof(x[p]) == 'undefined') {
+ 			return false;
+ 		}
+ 	}
+ 	for (p in this) {
+ 		if (this[p]) {
+ 			switch (typeof(this[p])) {
+ 			case 'object':
+ 				if (!this[p].equals(x[p])) {
+ 					return false;
+ 				}
+ 				break;
+ 			case 'function':
+ 				if (typeof(x[p]) == 'undefined' || (p != 'equals' && this[p].toString() != x[p].toString())) return false;
+ 				break;
+ 			default:
+ 				if (this[p] != x[p]) {
+ 					return false;
+ 				}
+ 			}
+ 		} else {
+ 			if (x[p]) return false;
+ 		}
+ 	}
+ 	for (p in x) {
+ 		if (typeof(this[p]) == 'undefined') {
+ 			return false;
+ 		}
+ 	}
+ 	return true;
+ };
 //命名空间
 var BASE_MAP = {
 	map: {},
