ef928bc2499dd641d06f7793a74b78a7090be5cf
chuduyunhuan <chuduyunhuan@163.com>
2017-04-06 14:51:12 +0800

    增加周的时间戳处理

--- WebContent/scripts/indexLeaflet.js
+++ WebContent/scripts/indexLeaflet.js
@@ -481,6 +481,71 @@ BASE_MAP.layers.setIcon = function(iconUrl,width,height){
 	});
 	return icon;
 };
+//输入年和周获取该周在该年的日期范围
+BASE_MAP.commonMethods.getDateRange = function(_year,_week) {
+	var beginDate;
+	var endDate;
+	if (_year == null || _year == '' || _week == null || _week == '') {
+		return "";
+	}
+	beginDate = getXDate(_year, _week, 1);
+	endDate = getXDate(_year, (_week - 0 + 1), 7);
+	return getNowFormatDate(beginDate) + " 至 " + getNowFormatDate(endDate);
+
+	function getNowFormatDate(theDate) {
+		var day = theDate;
+		var Year = 0;
+		var Month = 0;
+		var Day = 0;
+		var CurrentDate = "";
+		// 初始化时间   
+		Year = day.getFullYear(); // ie火狐下都可以   
+		Month = day.getMonth() + 1;
+		Day = day.getDate();
+		CurrentDate += Year + "-";
+		if (Month >= 10) {
+			CurrentDate += Month + "-";
+		} else {
+			CurrentDate += "0" + Month + "-";
+		}
+		if (Day >= 10) {
+			CurrentDate += Day;
+		} else {
+			CurrentDate += "0" + Day;
+		}
+		return CurrentDate;
+	}
+
+	//这个方法将取得某年(year)第几周(weeks)的星期几(weekDay)的日期   
+	function getXDate(year, weeks, weekDay) {
+		// 用指定的年构造一个日期对象，并将日期设置成这个年的1月1日   
+		// 因为计算机中的月份是从0开始的,所以有如下的构造方法   
+		var date = new Date(year, "0", "1");
+		// 取得这个日期对象 date 的长整形时间 time   
+		var time = date.getTime();
+		// 将这个长整形时间加上第N周的时间偏移   
+		// 因为第一周就是当前周,所以有:weeks-1,以此类推   
+		// 7*24*3600000 是一星期的时间毫秒数,(JS中的日期精确到毫秒)   
+		time += (weeks - 1) * 7 * 24 * 3600000;
+		// 为日期对象 date 重新设置成时间 time   
+		date.setTime(time);
+		return getNextDate(date, weekDay);
+	}
+	// 这个方法将取得 某日期(nowDate) 所在周的星期几(weekDay)的日期   
+	function getNextDate(nowDate, weekDay) {
+		// 0是星期日,1是星期一,...   
+		weekDay %= 7;
+		var day = nowDate.getDay();
+		var time = nowDate.getTime();
+		var sub = weekDay - day;
+		if (sub <= 0) {
+			sub += 7;
+		}
+		time += sub * 24 * 3600000;
+		nowDate.setTime(time);
+		return nowDate;
+	}
+};
 BASE_MAP.layers.setPopup = function(layer,content,opts){
 	var obj = {maxWidth: 1000, maxHeight: 800};
 	opts = BASE_MAP.commonMethods.setOptions(obj,opts);
